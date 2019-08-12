4c2678c3b81e439070559ae634d20610363c46c9
black <wangbo95o@163.com>
2019-08-12 08:33:36 +0800

    冲突演示

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("HelloWorld");
-		System.out.println("ÄãÊÇÒ»¸ö¹şÔú");
+		System.out.println("you are a blackpeople");
 	}	
 }
