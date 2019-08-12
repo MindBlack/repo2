4f8e35a782ef1a5e7071c0bfd81b727f6331169d
black <wangbo95o@163.com>
2019-08-12 09:18:05 +0800

    冲突测试

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
 	public static void main(String[] args){
 		System.out.println("HelloWorld");
 		System.out.println("like this is girl");
+		System.out.println("in the school");
 	}	
 }
